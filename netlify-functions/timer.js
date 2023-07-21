f63c1ff1d02cf20c556d6e13c81c319332df6e22
shanewalsh247 <shanewalsh247@gmail.com>
2023-07-21 14:42:01 +0100

    Update timer.js

--- netlify-functions/timer.js
+++ netlify-functions/timer.js
@@ -2,25 +2,6 @@
 
 const faunadb = require('faunadb');
 
-// new addition to get the highest time
-async function getHighestTime() {
-  try {
-    const { data } = await client.query(
-      q.Paginate(q.Match(q.Index('timer_by_action'), 'start'), { size: 1, reverse: true })
-    );
-
-    if (data.length > 0) {
-      return data[0].data;
-    } else {
-      return null;
-    }
-  } catch (error) {
-    console.error('Error retrieving highest time:', error);
-    return null;
-  }
-}
-
-
 // Configure FaunaDB client with your secret key
 const secret = 'fnAFJX8bt7AAUEle-qxXg08oTQ4RQkZlx4umrMz3'; // Replace with your actual FaunaDB secret key
 const q = faunadb.query;
@@ -31,7 +12,7 @@ exports.handler = async (event, context) => {
     // Parse the incoming request body
     const data = JSON.parse(event.body);
 
-    // Check the request type (start or reset)
+    // Check the request type (start, stop, or reset)
     if (data.action === 'start') {
       // Store the timer state in FaunaDB
       const timerState = {
@@ -50,14 +31,21 @@ exports.handler = async (event, context) => {
         body: JSON.stringify({ message: 'Timer started successfully.' })
       };
     } else if (data.action === 'stop') {
-      // Remove the timer state from FaunaDB
+      // Update the timer state in FaunaDB
       const timerRef = await client.query(
         q.Paginate(q.Match(q.Index('timer_by_action'), 'start'))
       );
-      
+
       if (timerRef.data.length > 0) {
         const ref = timerRef.data[0];
-        await client.query(q.Delete(ref));
+        const timerState = {
+          days: data.days,
+          hours: data.hours,
+          minutes: data.minutes,
+          seconds: data.seconds
+        };
+
+        await client.query(q.Update(ref, { data: timerState }));
 
         return {
           statusCode: 200,
@@ -74,10 +62,17 @@ exports.handler = async (event, context) => {
       const timerRef = await client.query(
         q.Paginate(q.Match(q.Index('timer_by_action'), 'start'))
       );
-      
+
       if (timerRef.data.length > 0) {
         const ref = timerRef.data[0];
-        await client.query(q.Delete(ref));
+        const timerState = {
+          days: 0,
+          hours: 0,
+          minutes: 0,
+          seconds: 0
+        };
+
+        await client.query(q.Update(ref, { data: timerState }));
 
         return {
           statusCode: 200,
@@ -87,6 +82,26 @@ exports.handler = async (event, context) => {
         return {
           statusCode: 404,
           body: JSON.stringify({ message: 'No timer found to reset.' })
+        };
+      }
+    } else if (data.action === 'getHighestTime') {
+      // Fetch the highest timer state from FaunaDB
+      const timerRef = await client.query(
+        q.Paginate(q.Match(q.Index('timer_by_action'), 'highest'))
+      );
+
+      if (timerRef.data.length > 0) {
+        const ref = timerRef.data[0];
+        const timerState = await client.query(q.Get(ref));
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify(timerState.data)
+        };
+      } else {
+        return {
+          statusCode: 404,
+          body: JSON.stringify({ message: 'Highest timer not found.' })
         };
       }
     } else {
